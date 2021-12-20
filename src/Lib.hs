4ddecbaa1cd69720a847414ff78480d5afbb83d6
beezee <brian.zeligson@gmail.com>
2021-12-19 23:51:02 -0500

    cruft

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,12 +3,8 @@ module Lib
       module Log,
       module Logger,
       module Safely,
-      someFunc
     ) where
 
 import Log
 import Logger
 import Safely
-
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
